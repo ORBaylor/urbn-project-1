b55b783e3dd8dd78bc56890e0ff5ba46780d9be3
Omar B <bay_omar@yahoo.com>
2022-04-25 00:32:33 -0400

    GeekJoke component

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <geek-jokes></geek-jokes>
+  <router-view/>
+</template>
+
+
+<script>
+
+import GeekJokes from '@/components/GeekJokes.vue'
+export default {
+  components: {
+    GeekJokes
+  }
+};
+
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/GeekJokes.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container">
+    <div>
+        <header>
+            Press Next For The Next Joke
+        </header>
+        <div class="center">
+          <h3>{{joke}}</h3>
+          <button>Next</button>
+        </div>
+    </div>
+    
+</div>
+  
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            joke:  "new test"
+        }
+    }
+}
+</script>
+
+<style>
+    .container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+        background-color: whitesmoke;
+        height: 100vh;
+    }
+</style>
